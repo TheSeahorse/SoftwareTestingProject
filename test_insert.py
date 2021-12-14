aa1db4d4e3fc691c787a5941b15d13d1c506c7e3
HamzaHaseeb01 <salmank4818@gmail.com>
2021-12-13 20:35:42 -0800

    update test cases

--- test_insert.py
+++ test_insert.py
@@ -2,53 +2,75 @@ import unittest
 
 from bs4 import BeautifulSoup
 
-# HTML From File
 with open("test.html", "r") as f:
 	doc = BeautifulSoup(f, "html.parser")
+# HTML From File
 
 
-class Test(unittest.TestCase):
+
+class TestReplaceWith(unittest.TestCase):
+		def test_replacewith_i(self):
+			new_tag=doc.new_tag("i")
+			new_tag.string="How to win friends and influence people with i tag"
+			doc.h1.replace_with(new_tag)
+			self.assertEqual(str(doc.i),'<i>How to win friends and influence people with i tag</i>')
+
+		def test_replacewith_h2(self):
+			new_tag=doc.new_tag("h3")
+			new_tag.string="Rich Dad Poor Dad with h3 tag"
+			doc.h1.replace_with(new_tag)
+			self.assertEqual(str(doc.h3),'<h3>Rich Dad Poor Dad with h3 tag</h3>')
+
+		def test_replacewth_bold(self):
+			new_tag=doc.new_tag("mark")
+			new_tag.string="You Are a Badass: How to Stop Doubting Your Greatness and Start Living an Awesome Life with mark bold"
+			doc.h1.replace_with(new_tag)
+			self.assertEqual(str(doc.mark),'<mark>You Are a Badass: How to Stop Doubting Your Greatness and Start Living an Awesome Life with mark bold</mark>')
+
+		def test_replacewith_small(self):
+			new_tag=doc.new_tag("small")
+			new_tag.string="The Power of Now: A Guide to Spiritual Enlightenment with small tag"
+			doc.h1.replace_with(new_tag)
+			self.assertEqual(str(doc.small),'<small>The Power of Now: A Guide to Spiritual Enlightenment with small tag</small>')
+
+
+class TestWrap(unittest.TestCase):
+
+	def test_wrap_with_div(self):
+		doc.p.wrap(doc.new_tag("div"))
+		self.assertEqual(str(doc.div),'<div><p class="story"><h2>line begins</h2></p></div>')
 	
-	def test(self):
-		self.assertEqual(doc.head.title.text,'Hamza Book Collection')
-	def test_find_tags_and_change_name(self):
-		self.assertEqual(len(doc.find_all("h1")),3)
-		doc.h1.name="blockquote"
-		self.assertEqual(len(doc.find('blockquote')),1)
-	def test_find_parent(self):
-		string_=doc.find(string='This is another line at the end')
-		self.assertEqual(string_.find_parent().name,"h2")
-	def test_find_next_siblings(self):
-		self.assertEqual(len(doc.h1.find_next_siblings('h1')),2)
-	def test_find_previous_siblings(self):
-		self.assertEqual(len(((doc.find('h1',id="3").find_previous_siblings('h1')))),2)
-	def test_wrap_tag(self):
-		doc.h1.wrap(doc.new_tag("div"))
-		self.assertEqual(len(doc.find('div')),1)
+	def test_wrap_with_b(self):
+		doc.title.string.wrap(doc.new_tag("b"))
+		self.assertEqual(str(doc.title),'<title><b>Hamza Book Collection</b></title>')
+
+
+class TestUnwrap(unittest.TestCase):
+
+	def test_unwrap_div(self):
+		doc.body.div.unwrap()
+		self.assertEqual(len(doc.find_all('div')),0)
+		print(doc)
+	def test_unwrap_bold(self):
+		doc.h6.label.unwrap()
+		self.assertEqual(str(doc.h6),'<h6>This is a medium header</h6>')
+
+		# doc.body.p.h2.unwrap()
+		# self.assertEqual(len(doc.find_all('h2')),0)
 	
-
 	
-
-
-
-
+class Test_smooth(unittest.TestCase):
+	def test_smooth_h2(self):
+		doc.h2.append(", from here")
+		doc.smooth()
+		doc.h2.prettify()
+		self.assertEqual(doc.h2.contents,['line begins, from here'])
 	
-
-
-
-	
-		# self.assertEqual(doc.find_parent(),)
-		
-		# self.assertEqual(doc.find_parent('h1'),'p')
-
-		# doc.h2.insert(1, " that has been appended!")
-		# self.assertEqual(str(doc.h2), "<h2>This is a Medium Header that has been appended!</h2>")
-		# doc.h2.insert(0, "Hej!")
-		# self.assertEqual(str(doc.h2), "<h2>Hej!This is a Medium Header that has been appended!</h2>")
-		# doc.h2.insert(1, " ")
-		# self.assertEqual(str(doc.h2), "<h2>Hej! This is a Medium Header that has been appended!</h2>")
-		# doc.h2.insert(5, "!!")
-		# self.assertEqual(str(doc.h2), "<h2>Hej! This is a Medium Header that has been appended!!!</h2>")
+	def test_smooth_title(self):
+		doc.title.b.append(", review the list for free")
+		doc.smooth()
+		doc.title.prettify()
+		self.assertEqual(doc.title.b.contents,['Hamza Book Collection, review the list for free'])
 
 
 if __name__ == "__main__":
