958cdcf934053c3c4fa579d86a6b60078835daac
rai-prashanna <prai931024@gmail.com>
2022-01-06 21:20:02 +0100

    added more logic

--- test_prai.py
+++ test_prai.py
@@ -35,6 +35,16 @@ class Testing(unittest.TestCase):
                 selector, ', '.join(expected_ids), ', '.join(element_ids)
             )
         )
+    def assertSelectsxml(self, selector, expected_ids, **kwargs):
+        element_ids = [el['id'] for el in self.soup_xml.select(selector, **kwargs)]
+        element_ids.sort()
+        expected_ids.sort()
+        self.assertEqual(expected_ids, element_ids,
+            "Selector %s, expected [%s], got [%s]" % (
+                selector, ', '.join(expected_ids), ', '.join(element_ids)
+            )
+        )
+    assertSelectxml = assertSelectsxml
 
     assertSelect = assertSelects
 
@@ -42,26 +52,37 @@ class Testing(unittest.TestCase):
         for selector, expected_ids in tests:
             self.assertSelect(selector, expected_ids)
 
-    def test_one_tag_one(self):
-        # 1,2,3,4,5,6,10
+    def test_select_one_tag_one(self):
+        """Test-Case that expects ."""
+        """Test path [1,2,3,4,5,6,10]"""
         elements = self.soup_html.select('title')
         self.assertEqual(len(elements), 1)
         self.assertEqual(elements[0].name, 'title')
         self.assertEqual(elements[0].contents, ['title'])
 
 
-    def test_limit(self):
-        # 1,2,3,4,6,10
+    def test_select_elements_with_limit(self):
+        """Test-Case that expects ."""
+        """Test path [1,2,3,4,6,10]"""
         self.assertSelects('html div', ['main'], limit=1)
-    # #attrs = {"class": "sister"}
 
-    def test_find_by_namespaced_name(self):
-        #[1,2,4,5,6,10]
+    def test_select_namespaced(self):
+        """Test-Case that expects ."""
+        """Test path [1,2,4,5,6,10]"""
         namespaces = dict(first="http://namespace1/", second="http://namespace2/")
-        self.soup_xml.select("second|child", namespaces=namespaces)
+        self.soup_xml.select("child", namespaces=namespaces)
+        result=self.soup_xml.select("child", namespaces=namespaces)
+        resultsetlist=[]
+        for tag in result:
+            resultsetlist.append(str(tag))
+        self.assertEqual(["<ns1:child>I'm in namespace 1</ns1:child>", "<ns2:child>I'm in namespace 2</ns2:child>"],resultsetlist)
 
-    def test_find_by_namespaced_name1(self):
-        #[1, 2, 4, 5, 6, 10]
+    def test_select_namespaced_with_limit(self):
+        """Test-Case that expects ."""
+        """Test path [1,2,4,5,6,10]"""
         namespaces = dict(first="http://namespace1/", second="http://namespace2/")
-        self.soup_xml.select("second|child", namespaces=namespaces,limit=1)
-
+        result=self.soup_xml.select("child", namespaces=namespaces,limit=1)
+        resultsetlist=[]
+        for tag in result:
+            resultsetlist.append(str(tag))
+        self.assertEqual(["<ns1:child>I'm in namespace 1</ns1:child>"],resultsetlist)
